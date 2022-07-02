e9c67f9670999c512762fe78c04b9c75e2b41427
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-07-01 23:22:37 -0400

    Setting up aesthetics for pass-gen

---
+++ random-/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import Intro from "./Components/Intro.js"
+
+function App() {
+  return (
+    <div className="App">
+    <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ random-/src/Components/Intro.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function intro(){
+    return(
+        <div>
+        <h1>Generate a random password</h1>
+        <h4>Never use an insecure password again.</h4>
+        <br></br>
+        <button type="button" class="btn btn-success">Generate Passwords</button>
+
+
+        </div>
+
+
+
+    )
+}
+
+export default intro
