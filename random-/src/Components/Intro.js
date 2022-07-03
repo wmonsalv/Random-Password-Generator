ad2ae96a000b6dd0637012e6daff7e7d2fe8d22b
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-07-02 22:57:05 -0400

    front end final touches

--- random-/src/Components/Intro.js
+++ random-/src/Components/Intro.js
@@ -6,13 +6,13 @@ function intro(){
         <h1><span>Generate a</span> random password</h1>
         <h4>Never use an insecure password again.</h4>
         <br></br>
-        <button type="button" class="btn btn-success">Generate Passwords</button>
-
-
+        <button type="button" id="passwordGenerator" class="btn btn-success">Generate Passwords</button>
+        <hr id="horz"/>
+        <form>
+        <input class="form-control" id="myForm" type="text" placeholder="Random Generated password number 1"/>
+        <input class="form-control" id="myForm" type="text" placeholder="Random Generated password number 2"/>
+        </form>
         </div>
-
-
-
     )
 }
 
