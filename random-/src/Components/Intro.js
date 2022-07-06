5782918e6068e0d126724ec56e2d6bde1776cd5f
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-07-06 12:51:42 -0400

    Update Intro.js

--- random-/src/Components/Intro.js
+++ random-/src/Components/Intro.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react"
-
+import copy from "copy-to-clipboard";  
 
 function Intro(){
 
   const url = "https://passwordinator.herokuapp.com/generate?num=true&caps=true&char=true&len=15"
 
   const[password, setPassword] = useState([{}])
-
+    
+   
    const fetchAPI = () => {
      fetch(url).then(
        response => response.json()
@@ -16,7 +17,6 @@ function Intro(){
        }
      )
    }
-   
 
    let myElement = document.getElementById("pass")
 
@@ -31,8 +31,8 @@ function Intro(){
         <hr id="horz"/>
         <div>
         <div id="pass" className="pw-display">{password.data}</div>
-        <button type="button" class="btn btn-light copy">Copy</button>
         </div>
+        
         </div>
     )
 }
