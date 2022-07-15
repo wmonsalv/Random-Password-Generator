b061bf412e1c80816c2a52f92fa6e2a988843e34
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-07-14 23:59:52 -0400

    more changes

--- random-/src/Components/PasswordForm.js
+++ random-/src/Components/PasswordForm.js
@@ -3,18 +3,45 @@ import {useState} from "react"
 
 function PasswordGenerator() {
 
-    const [charCount, setCharCount] = useState("")
-    // const [digitCount, setDigitCount] = useState("")
-    // const [symbolCount, setSymbolCount] = useState("")
 
-    const onCharChange = (e) => {
-        const charValue = e.target.value
+    const [charCount, setCharCount] = useState("")
+    const [digitCount, setDigitCount] = useState("")
+    const [symbolCount, setSymbolCount] = useState("")
+
+    const onCharChange = (event) => {
+        const charValue = event.target.value
         setCharCount(charValue)
+    }
+
+    const onDigitChange = (event) => {
+        const dValue = event.target.value
+        setDigitCount(dValue)
+    }
+
+    const onSymbolChange = (event) => {
+        const symbolValue = event.target.value
+        setSymbolCount(symbolValue)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const formData = {
+            characterCount: charCount,
+            numberCount: digitCount,
+            symbolsCount: symbolCount
+        }
+
+        setCharCount("")
+        setDigitCount("")
+        setSymbolCount("")
+
+        console.log(formData)
     }
 
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
             <div className="form-group">
                     <label id="labelF" for="exampleFormControlSelect1">Char count</label>
                     <select value={charCount} onChange={onCharChange} className="form-control" id="exampleFormControlSelect1">
@@ -32,7 +59,7 @@ function PasswordGenerator() {
                 </div>
                 <div className="form-group">
                     <label id="labelF" for="exampleFormControlSelect1">Digit count</label>
-                    <select className="form-control" id="exampleFormControlSelect1">
+                    <select className="form-control" value={digitCount} onChange={onDigitChange} id="exampleFormControlSelect1">
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -47,7 +74,7 @@ function PasswordGenerator() {
                 </div>
                 <div className="form-group">
                     <label id="labelF" for="exampleFormControlSelect1">Symbol count</label>
-                    <select className="form-control" id="exampleFormControlSelect1">
+                    <select className="form-control" value={symbolCount} onChange={onSymbolChange} id="exampleFormControlSelect1">
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
